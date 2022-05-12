5cd5e25e7a82c2bf10c1123a652592df9f507a5a
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-11 17:51:53 -0700

    added new font family too

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/routes/**/*.{svelte,js,ts}'],
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
     themes: ['light', 'dracula'],
     darkTheme: 'dark'
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto Flex', ...defaultTheme.fontFamily.sans]
+      }
+    }
   }
 };
